457b59d806d4b95c088923d668b113b3f5efb397
Your Name <kishorebabu.mc@gmail.com>
2017-02-10 16:13:29 +0530

    admin UI

--- index.php
+++ index.php
@@ -20,36 +20,53 @@
 	}
 ?>
 <!DOCTYPE html>
-<html land ="en">
-	<head>
-		<link href="css/bootstrap.min.css" rel="stylesheet">
-		<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
-		<link href="css/dcao.css" rel="stylesheet">
-		<title>Audit Programme Dashboard</title>
-	</head>
-	<body>
-		<div class="container">
-			<div class="row">
-				<div class="col-md-offset-5 col-md-3">
-					<div class="form-login">
-						<form action="" method="post">
-						<h4>Welcome back, <br> Audit Management Software</h4>
-						<input type="text" name="user" class="form-control input-sm chat-input" placeholder="Username" autocomplete="off" /></br>
-						<input type="password" name="pass" class="form-control input-sm chat-input" placeholder="Password" /></br>
-						<div class="wrapper">
-							<div><p style="font-family:Roboto; color:Red; font-size: 12px;"> <?php echo $error; ?> </p></div>
-							<span class="group-btn">     
-								<button type ="submit" class="btn btn-primary btn-md">login <i class="fa fa-sign-in"></i></button>
-							</span>
-						</div>
-						</form>						
-					</div>
-				
-				</div>
-			</div>
-		</div>
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-		<script src="js/bootstrap.min.js"></script>
-	</body>
-</html>	
-	
+<html >
+<head>
+  <meta charset="UTF-8">
+  <title>Audit Programme Management Portal</title>
+  
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
+
+  <link rel='stylesheet prefetch' href='http://fonts.googleapis.com/css?family=Roboto:400,100,300,500,700,900|RobotoDraft:400,100,300,500,700,900'>
+<link rel='stylesheet prefetch' href='http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css'>
+
+      <link rel="stylesheet" href="css/style1.css">
+
+  
+</head>
+
+<body>
+  
+<!-- Form Mixin-->
+<!-- Input Mixin-->
+<!-- Button Mixin-->
+<!-- Pen Title-->
+<div class="pen-title">
+  <h1>Audit Programme</h1><span>Management Portal by <a href=''>Kishore Babu</a></span>
+</div>
+<!-- Form Module-->
+<div class="module form-module">
+  <div class="toggle"><i class="fa fa-times fa-pencil"></i>
+    <div class="tooltip">Click Me</div>
+  </div>
+  <div class="form">
+    <h2>Login to your account</h2>
+    <form action="" method="post">
+      <input type="text" name="user" placeholder="Username" autocomplete="off"/>
+      <input type="password" name="pass" placeholder="Password"/>
+      <button>Login</button>
+    </form>
+  </div>
+  
+  <div class="form">
+    <h2>Designed and Developed by Kishore Babu, Junior Inspector</h2>
+  </div>
+  <div class="cta" style = "color:red"><?php echo $error; ?></div>
+</div>
+<script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+
+
+    <script src="js/index1.js"></script>
+
+</body>
+</html>
